765ef646d7a1f5f9d10f4f8773098064a2816425
danielamichellod <danielam_22@apple.com>
2025-09-24 16:28:06 +0200

    limpieza de datos del fichero customer

---
+++ SRC/sp_limpieza_customer.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def eda_preliminar(df):
+    
+    
+    display(df.sample(5))
+    
+    print('----------------')
+    
+    print('INFO')
+    
+    display(df.info())
+    
+    print('----------------')
+    
+    print('NULOS')
+    
+    display(round(df.isnull().sum()/df.shape[0]*100,2))
+    
+    print('----------------')
+    
+    print('DUPLICADOS')
+    
+    print(df.duplicated().sum())
+    
+    print('----------------')
+    
+    print('VALUE COUNTS')
+    
+    for col in df.select_dtypes(include ='O').columns:
+        print(df[col].value_counts())
+        print('---------------------------')
